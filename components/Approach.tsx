2728cd66c248ca847c8ae9f194c2f6e7ca09d8e7
Phirawit Tungjaroenvorrakhun <168079020+Phirawit-Journey@users.noreply.github.com>
2024-07-20 00:16:24 +0700

    change button style

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -116,9 +116,9 @@ const Card = ({
 const AceternityIcon = ({ order }: { order: string }) => {
   return (
     <div>
-      <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-2  text-purple backdrop-blur-3xl text-2xl font-bold ">
+      <button className="p-[1px] relative inline-flex ">
+        <span className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+        <span className="inline-flex px-7 py-3  bg-black rounded-lg  relative group transition duration-200 text-white hover:bg-transparent text-2xl font-bold ">
           {order}
         </span>
       </button>
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -14,13 +14,10 @@ const MagicButton = ({
   otherClasses?: string;
 }) => {
   return (
-    <button
-      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none"
-      onClick={handleClick}
-    >
-      <div className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+    <button className="p-[1px] relative inline-flex " onClick={handleClick}>
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
       <div
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-5 ${otherClasses}`}
+        className={`inline-flex px-7 py-3  bg-black rounded-lg  relative group transition duration-200 text-white hover:bg-transparent gap-5 ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
@@ -33,10 +30,10 @@ const MagicButton = ({
 export default MagicButton;
 
 {
-  /* <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-    Border Magic
-  </span>
+  /* <button className="p-[3px] relative">
+  <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+  <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+    Lit up borders
+  </div>
 </button> */
 }
